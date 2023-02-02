0fb033dded2d262f28b196906a1cc71ea58fde37
Fossa88 <90715717+Fossa88@users.noreply.github.com>
2023-02-02 10:03:33 -0800

    Add files via upload

---
+++ Projects/RPS Game.py
@@ -0,0 +1,45 @@
+print("- - - - - - - - - - - - - - - - - - - - - - - - -")
+print("Computer Plays Rock Paper Scissors Against Itself")
+print("=================================================")
+
+import random
+Ranblue = random.randint(1,3)
+Ranred = random.randint(1,3)
+Ranblue1 = ""
+Ranred1 = ""
+
+if Ranblue == 1:
+    Ranblue1 = "Scissors"
+elif Ranblue == 2:
+    Ranblue1 = "Paper"
+elif Ranblue == 3:
+    Ranblue1 = "Rock"
+
+if Ranred == 1:
+    Ranred1 = "Scissors"
+elif Ranred == 2:
+    Ranred1 = "Paper"
+elif Ranred == 3:
+    Ranred1 = "Rock"
+
+blue = Ranblue
+red = Ranred
+
+print('Blue team has ' + Ranblue1)
+print('Red team has ' + Ranred1)
+if Ranblue1 == Ranred1:
+    print("The game is a tie")
+if Ranblue1 == "Scissors":
+    if Ranred1 == "Rock":
+        print("The red team wins the game")
+    elif Ranred1 == "Paper":
+        print("The blue team wins the game")
+if Ranred1 == "Scissors":
+    if Ranblue1 == "Rock":
+        print("The blue team wins the game")
+    elif Ranblue1 == "Paper":
+        print("The red team wins the game")
+
+    
+
+print("=================================================")
