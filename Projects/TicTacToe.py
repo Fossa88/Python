50da88e112ccf26720981657ed9728729d7c0fa3
Fossa88 <90715717+Fossa88@users.noreply.github.com>
2023-02-02 13:28:06 -0800

    Add files via upload

---
+++ Projects/TicTacToe.py
@@ -0,0 +1,29 @@
+print("TicTacToe Game")
+print("- - - - -")
+print("To Hear the Rules Input 1")
+print("To Start the Game Input 2")
+print("- - - - -")
+
+X = input()
+if X == 1:
+        print("You will be playing as X's, to input a place on the grid you will input a cordinate on the grid.")
+        print("========================================")
+        print(" A1 | B1 | C1 ")
+        print("--------")
+        print(" A2 | B2 | C2 ")
+        print("--------")
+        print(" A3 | B3 | C3 ")
+        print("========================================")
+
+
+
+if X == 2:
+    print("========================================")
+
+    print("  |  |  ")
+    print("--------")
+    print("  |  |  ")
+    print("--------")
+    print("  |  |  ")
+
+    print("========================================")
