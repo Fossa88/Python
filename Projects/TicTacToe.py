c8f94aa59a1437b01d34314197db07fbc9aaa431
Fossa88 <90715717+Fossa88@users.noreply.github.com>
2023-02-09 11:24:27 -0800

    Update TicTacToe.py

--- Projects/TicTacToe.py
+++ Projects/TicTacToe.py
@@ -1,29 +1,26 @@
-print("TicTacToe Game")
 print("- - - - -")
-print("To Hear the Rules Input 1")
-print("To Start the Game Input 2")
+print("Welcome to the TicTacToe Game by Fossa")
+print("To Hear the Rules Input 1, To Start the Game Input 2 ")
 print("- - - - -")
 
-X = input()
-if X == 1:
-        print("You will be playing as X's, to input a place on the grid you will input a cordinate on the grid.")
-        print("========================================")
-        print(" A1 | B1 | C1 ")
-        print("--------")
-        print(" A2 | B2 | C2 ")
-        print("--------")
-        print(" A3 | B3 | C3 ")
-        print("========================================")
-
-
-
-if X == 2:
+Uinp = input()
+if Uinp == 1: 
+    print("You will be playing as X's, to input a place on the grid you will input a cordinate on the grid.")
+    print("========================================")
+    print(" A1 | B1 | C1 ")
+    print("--------")
+    print(" A2 | B2 | C2 ")
+    print("--------")
+    print(" A3 | B3 | C3 ")
     print("========================================")
 
-    print("  |  |  ")
-    print("--------")
-    print("  |  |  ")
-    print("--------")
-    print("  |  |  ")
+else:
+    print("========================================")
+
+    print("   |   |   ")
+    print("-----------")
+    print("   |   |   ")
+    print("-----------")
+    print("   |   |   ")
 
     print("========================================")
