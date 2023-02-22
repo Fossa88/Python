271ff88a4a40fe43e6065c6865ac2d87689b8254
Fossa88 <90715717+Fossa88@users.noreply.github.com>
2023-02-22 13:33:01 -0800

    Update Basic-Measurement-Converter.py

--- Projects/Basic-Measurement-Converter.py
+++ Projects/Basic-Measurement-Converter.py
@@ -9,56 +9,56 @@ def Menu():
 def KMtM():
     print('You selected Km to m')
     num = float(input('Enter value km: '))
-    num1 = num*1000
+    num1 = round(num*1000, 1)
     print('{}m' .format(num1))
     num = ''
 
 def MtKM():
     print('You selected m to Km')
     num2 = float(input('Enter value m: '))
-    num3 = num2/1000
+    num3 = round(num2/1000, 1)
     print('{}km' .format(num3))
     num2 = ''
 
 def MtCM():
     print('You selected m to cm')
     num4 = float(input('Enter value m: '))
-    num5 = num4*100
+    num5 = round(num4*100, 1)
     print('{}cm' .format(num5))
     num4 = ''
 
 def CMtM():
     print('You selected cm to m')
     num6 = float(input('Enter value cm: '))
-    num7 = num6/100
+    num7 = round(num6/100, 1)
     print('{}m' .format(num7))
     num6 = ''
 
 def CMtMM():
     print('You selected cm to mm')
     num8 = float(input('Enter value cm: '))
-    num9 = num8*10
+    num9 = round(num8*10, 1)
     print('{}mm' .format(num9))
     num8 = ''
 
 def MMtCM():
     print('You selected mm to cm')
     num10 = float(input('Enter value mm: '))
-    num11 = num10/10
+    num11 = round(num10/10, 1)
     print('{}cm' .format(num11))
     num10 = ''
 
 def CtF():
     print('You selected Celcius to Fahrenheit')
     num12 = float(input('Enter value C: '))
-    num13 = (num12*1.8)+32
+    num13 = round((num12*1.8)+32, 1)
     print('{} Fahrenheit' .format(num13))
     num12 = ''
 
 def FtC():
     print('You selected Fahrenheit to Celcius')
     num14 = float(input('Enter value F: '))
-    num15 = (num14/1.8)-32
+    num15 = round((num14/1.8)-32, 1)
     print('{} Celcius' .format(num15))
     num14 = ''
 
