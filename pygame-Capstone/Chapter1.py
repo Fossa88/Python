95562006a1a8339915ba4fe3a3f957e2d2efcedc
Fossa88 <90715717+Fossa88@users.noreply.github.com>
2023-05-08 13:34:28 -0700

    Update Chapter1.py

--- pygame-Capstone/Chapter1.py
+++ pygame-Capstone/Chapter1.py
@@ -5,23 +5,26 @@ pygame.init()
 running = True
 (width, height) = (600, 400)
 surface = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Chapter 1: The intro')
+pygame.display.set_caption('Chapter 1: The intro')  
 
 clock = pygame.time.Clock()
 fps_limit = 60
+    #functions
+
+    #Img stuff
+GurtrudeImg = pygame.image.load('GurtrudeImgs/GurtrudeDown.png')
+def Gurtrude(X,Y):
+    surface.blit(GurtrudeImg, (X,Y))    
+Xpos = 150
+Ypos = 150
+Vel = 5
 
     # Colours
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 AMARANTH = (159, 43, 104)
-
-    # Player Rectangle
-colour = BLUE
-Xpos = 300
-Ypos = 200
-Vel = 2
-player = pygame.draw.rect(surface, colour, pygame.Rect(Xpos, Ypos, 30, 30))
+BaseCol = AMARANTH
 
     # Rectangle for collision detection
 colour1 = GREEN
@@ -31,6 +34,16 @@ Size = random.randint(1,100)
 Rectangle = pygame.draw.rect(surface, colour1, pygame.Rect(Xpos1, Ypos1, Size, Size))
 
 
+
+    #Big Collision functions block
+def CollisionCheck(Sprite1, Sprite2):
+    Bool = pygame.sprite.collide_rect(Sprite1, Sprite2)
+    if Bool == True:
+        BaseCol = BLUE
+
+def MoveLeft():
+    Xpos == Xpos + 2
+
 while running:
     clock.tick(fps_limit)
             
@@ -38,29 +51,32 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        #Movement
     keys = pygame.key.get_pressed()
-    
     if keys[pygame.K_a]:
         Xpos -= Vel
-
+        GurtrudeImg = pygame.image.load('GurtrudeImgs/GurtrudeLeft.png')
     if keys[pygame.K_d]:
         Xpos += Vel
-
+        GurtrudeImg = pygame.image.load('GurtrudeImgs/GurtrudeRight.png')
     if keys[pygame.K_w]:
         Ypos -= Vel
-
+        GurtrudeImg = pygame.image.load('GurtrudeImgs/GurtrudeUp.png')
     if keys[pygame.K_s]:
         Ypos += Vel
-                
-        
-    if player.colliderect(Rectangle):
-        colour1 = RED
-    else:
-        colour1 = GREEN
+        GurtrudeImg = pygame.image.load('GurtrudeImgs/GurtrudeDown.png')
+    if Xpos < -0:
+        Xpos = 1 
+    if Xpos > 590:
+        Xpos = 589
 
-    surface.fill(AMARANTH)
-    player = pygame.draw.rect(surface, colour, pygame.Rect(Xpos, Ypos, 30, 30))
+                
+        #collisions
+
+    surface.fill(BaseCol)
     Rectangle = pygame.draw.rect(surface, colour1, pygame.Rect(Xpos1, Ypos1, Size, Size))
+        #Img blits
+    Gurtrude(Xpos,Ypos) 
 
     pygame.display.flip()
 
